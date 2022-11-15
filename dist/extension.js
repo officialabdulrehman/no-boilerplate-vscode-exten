83325b316dd2dcc148035a45dcc601bf065614f7
NizTheDev <arehman08@hotmail.com>
2022-11-15 22:02:06 +0500

    feat: thought out the basic structure

--- dist/extension.js
+++ dist/extension.js
@@ -9,370 +9,15 @@ module.exports = require("vscode");
 
 /***/ }),
 /* 2 */
-/***/ ((__unused_webpack_module, exports, __webpack_require__) => {
-
-
-Object.defineProperty(exports, "__esModule", ({ value: true }));
-exports.main = void 0;
-const files_service_1 = __webpack_require__(3);
-const main = (args) => {
-    args.forEach((title) => new files_service_1.StaticFileService(title, true));
-};
-exports.main = main;
-/**
- * user types command Snippet create
- * input box opens
- * user types names with spaces
- * parse
- * create files
- */
-/**
- * user types command Snippet create2
- * 1st input box opens
- * user types names with spaces
- * 2nd input box opens for derived service classes and routers ( input => profile, selfProfile )
- * parse
- * create files
- */ 
-
-
-/***/ }),
-/* 3 */
-/***/ ((__unused_webpack_module, exports, __webpack_require__) => {
-
-
-Object.defineProperty(exports, "__esModule", ({ value: true }));
-exports.StaticFileService = void 0;
-const assert = __webpack_require__(4);
-const node_fs_1 = __webpack_require__(5);
-const dao_template_1 = __webpack_require__(6);
-const dto_template_1 = __webpack_require__(7);
-const model_template_1 = __webpack_require__(8);
-const router_template_1 = __webpack_require__(9);
-const service_template_1 = __webpack_require__(10);
-const common_1 = __webpack_require__(11);
-const path = __webpack_require__(12);
-class StaticFileService {
-    constructor(title, autoCreate = true) {
-        this._titleSmall = null;
-        this._titleCapital = null;
-        this._dto = null;
-        this._model = null;
-        this._dao = null;
-        this._service = null;
-        this._router = null;
-        this.throwIfInvalidTitle(title);
-        this.setTitleSmall(title);
-        this.setTitleCapital(title);
-        this.loadTemplates();
-        if (autoCreate) {
-            this.init();
-        }
-    }
-    init() {
-        this.createDTO();
-        this.createModel();
-        this.createDAO();
-        this.createService();
-        this.createRouter();
-    }
-    createDAO() {
-        this.create(this._dao);
-    }
-    createDTO() {
-        this.create(this._dto);
-    }
-    createModel() {
-        this.create(this._model);
-    }
-    createService() {
-        this.create(this._service);
-    }
-    createRouter() {
-        this.create(this._router);
-    }
-    //   Internal Methods below
-    throwIfInvalidTitle(title) {
-        assert(title.trim(), new Error("Invalid input"));
-    }
-    setTitleSmall(title) {
-        this._titleSmall = (0, common_1.decapitallizeFirstLetter)(title);
-    }
-    setTitleCapital(title) {
-        this._titleCapital = (0, common_1.capitallizeFirstLetter)(title);
-    }
-    create({ folderPath, filePath, fileContent, fileIPath, fileIContent }) {
-        (0, node_fs_1.mkdirSync)(path.join(__dirname, folderPath), { recursive: true });
-        (0, node_fs_1.appendFileSync)(path.join(__dirname, filePath), fileContent);
-        if (fileIPath && fileIContent) {
-            (0, node_fs_1.appendFileSync)(path.join(__dirname, fileIPath), fileIContent);
-        }
-    }
-    loadTemplates() {
-        this.setDTO();
-        this.setModel();
-        this.setDAO();
-        this.setService();
-        this.setRouter();
-    }
-    setDTO() {
-        this._dto = (0, dto_template_1.datamodelTemplate)({
-            varSmall: this._titleSmall,
-            varCapital: this._titleCapital
-        });
-    }
-    setModel() {
-        this._model = (0, model_template_1.modelTemplate)({
-            varSmall: this._titleSmall,
-            varCapital: this._titleCapital
-        });
-    }
-    setDAO() {
-        this._dao = (0, dao_template_1.daoTemplate)({
-            varSmall: this._titleSmall,
-            varCapital: this._titleCapital
-        });
-    }
-    setService() {
-        this._service = (0, service_template_1.serviceTemplate)({
-            varSmall: this._titleSmall,
-            varCapital: this._titleCapital
-        });
-    }
-    setRouter() {
-        this._router = (0, router_template_1.routerTemplate)({
-            varSmall: this._titleSmall,
-            varCapital: this._titleCapital
-        });
-    }
-}
-exports.StaticFileService = StaticFileService;
-
-
-/***/ }),
-/* 4 */
-/***/ ((module) => {
-
-module.exports = require("assert");
-
-/***/ }),
-/* 5 */
 /***/ ((module) => {
 
 module.exports = require("node:fs");
 
 /***/ }),
-/* 6 */
-/***/ ((__unused_webpack_module, exports) => {
-
-
-Object.defineProperty(exports, "__esModule", ({ value: true }));
-exports.daoTemplate = void 0;
-const daoTemplate = ({ varSmall, varCapital }) => {
-    const folderPath = `src/dao/${varSmall}`;
-    const filePath = `src/dao/${varSmall}/${varSmall}.dao.ts`;
-    const fileContent = `
-import { MongoDAO } from "../MongoDAO";
-import { ${varCapital}DTO } from "../../datamodels/${varSmall}/${varCapital}.dto";
-import { ${varCapital}Model } from "../../models/${varSmall}/${varSmall}.model";
-
-import { ${varCapital}DAOI } from "./${varSmall}.daoI";
-import { query } from "express";
-import { IPaginateResult } from "../pagination";
-
-export class ${varCapital}DAO extends MongoDAO<${varCapital}DTO> implements ${varCapital}DAOI {
-
-}
-
-export const ${varSmall}DAO = new ${varCapital}DAO(${varCapital}Model, new ${varCapital}DTO());
-
-`.trim();
-    const fileIPath = `src/dao/${varSmall}/${varSmall}.daoI.ts`;
-    const fileIContent = `
-import {DAOI} from "../daoI";
-import {${varCapital}DTO} from "../../datamodels/${varSmall}/${varCapital}.dto";
-
-export type ${varCapital}DAOI = DAOI<${varCapital}DTO>
-
-`.trim();
-    return {
-        folderPath,
-        filePath,
-        fileContent,
-        fileIPath,
-        fileIContent
-    };
-};
-exports.daoTemplate = daoTemplate;
-
-
-/***/ }),
-/* 7 */
-/***/ ((__unused_webpack_module, exports) => {
-
-
-Object.defineProperty(exports, "__esModule", ({ value: true }));
-exports.datamodelTemplate = void 0;
-const datamodelTemplate = ({ varSmall, varCapital }) => {
-    const folderPath = `src/datamodels/${varSmall}`;
-    const filePath = `src/datamodels/${varSmall}/${varCapital}.dto.ts`;
-    const fileContent = `
-import { DTO } from "../dto";
-
-export class ${varCapital}DTO extends DTO {
-
-}
-
-`.trim();
-    return {
-        folderPath,
-        filePath,
-        fileContent
-    };
-};
-exports.datamodelTemplate = datamodelTemplate;
-
-
-/***/ }),
-/* 8 */
-/***/ ((__unused_webpack_module, exports) => {
-
-
-Object.defineProperty(exports, "__esModule", ({ value: true }));
-exports.modelTemplate = void 0;
-const modelTemplate = ({ varSmall, varCapital }) => {
-    const folderPath = `src/models/${varSmall}`;
-    const filePath = `src/models/${varSmall}/${varSmall}.model.ts`;
-    const fileContent = `
-import { model, Schema, Document, Types } from "mongoose";
-
-import { DTOCreate } from "../../datamodels/dto";
-import { ${varCapital}DTO } from "../../datamodels/${varSmall}/${varCapital}.dto";
-
-type ID = Types.ObjectId
-
-
-export interface I${varCapital}Doc extends DTOCreate<${varCapital}DTO>, Document { }
-
-
-const schemaFields: Record<keyof DTOCreate<${varCapital}DTO>, any> = {
-
-};
-
-
-const schema = new Schema(schemaFields, { timestamps: true });
-
-export const ${varCapital}Model = model<I${varCapital}Doc>("${varCapital}", schema);
-
-`.trim();
-    return {
-        folderPath,
-        filePath,
-        fileContent
-    };
-};
-exports.modelTemplate = modelTemplate;
-
-
-/***/ }),
-/* 9 */
-/***/ ((__unused_webpack_module, exports) => {
-
-
-Object.defineProperty(exports, "__esModule", ({ value: true }));
-exports.routerTemplate = void 0;
-const routerTemplate = ({ varSmall, varCapital }) => {
-    const folderPath = `src/routers/${varSmall}`;
-    const filePath = `src/routers/${varSmall}/${varSmall}.router.ts`;
-    const fileContent = `
-import { Request, Response, NextFunction } from "express";
-import { check, sanitize, oneOf } from "express-valueidator";
-
-import { apiError, catchAsync, apiValidation, apiOk, IQuery } from "../../util/apiHelpers";
-import { GetIdentity } from "../../config/auth";
-
-import { RouterClass } from "../resource/RouterClass";
-
-import { ResourceRouter } from "../resource/ResourceRouter";
-
-import { ${varSmall}Service } from "../../services/${varSmall}/${varSmall}.service";
-import { ${varCapital}DTO } from "../../datamodels/${varSmall}/${varCapital}.dto";
-
-import _ from "lodash";
-
-class ${varCapital}Router extends ResourceRouter<${varCapital}DTO> {
-
-}
-
-export const ${varSmall}Router = new ${varCapital}Router(new ${varCapital}DTO(), ${varSmall}Service);
-export default ${varSmall}Router;
-`.trim();
-    return {
-        folderPath,
-        filePath,
-        fileContent
-    };
-};
-exports.routerTemplate = routerTemplate;
-
-
-/***/ }),
-/* 10 */
-/***/ ((__unused_webpack_module, exports) => {
-
-
-Object.defineProperty(exports, "__esModule", ({ value: true }));
-exports.serviceTemplate = void 0;
-const serviceTemplate = ({ varSmall, varCapital }) => {
-    const folderPath = `src/services/${varSmall}`;
-    const filePath = `src/services/${varSmall}/${varSmall}.service.ts`;
-    const fileContent = `import { ServiceCRUD } from "../service.crud";
-import { ${varCapital}DAOI } from "../../dao/${varSmall}/${varSmall}.daoI";
-import { ${varSmall}DAO } from "../../dao/${varSmall}/${varSmall}.dao";
-import { ${varCapital}DTO } from "../../datamodels/${varSmall}/${varCapital}.dto";
-import { IPaginateResult } from "../../dao/pagination";
-
-class ${varCapital}Service extends ServiceCRUD<${varCapital}DTO, ${varCapital}DAOI> {
-
-
-
-}
-
-
-export const ${varSmall}Service = new ${varCapital}Service(${varSmall}DAO);
-export default ${varSmall}Service;
-`.trim();
-    return {
-        folderPath,
-        filePath,
-        fileContent
-    };
-};
-exports.serviceTemplate = serviceTemplate;
-
-
-/***/ }),
-/* 11 */
-/***/ ((__unused_webpack_module, exports) => {
-
-
-Object.defineProperty(exports, "__esModule", ({ value: true }));
-exports.decapitallizeFirstLetter = exports.capitallizeFirstLetter = void 0;
-const capitallizeFirstLetter = (str) => {
-    return `${str.charAt(0).toUpperCase()}${str.slice(1)}`;
-};
-exports.capitallizeFirstLetter = capitallizeFirstLetter;
-const decapitallizeFirstLetter = (str) => {
-    return `${str.charAt(0).toLowerCase()}${str.slice(1)}`;
-};
-exports.decapitallizeFirstLetter = decapitallizeFirstLetter;
-
-
-/***/ }),
-/* 12 */
+/* 3 */
 /***/ ((module) => {
 
-module.exports = require("path");
+module.exports = require("node:path");
 
 /***/ })
 /******/ 	]);
@@ -412,8 +57,10 @@ exports.deactivate = exports.activate = void 0;
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 const vscode = __webpack_require__(1);
-const main_1 = __webpack_require__(2);
+const node_fs_1 = __webpack_require__(2);
+const path = __webpack_require__(3);
 __dirname = __dirname.replace("dist", '');
+(0, node_fs_1.mkdirSync)(path.join(__dirname, ".no-boilerplate"), { recursive: true });
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 function activate(context) {
@@ -427,15 +74,58 @@ function activate(context) {
         // The code you place here will be executed every time your command is executed
         // Display a message box to the user
         vscode.window.showInformationMessage('Hello World from no-boilerplate!');
-        vscode.workspace.fs;
-        (0, main_1.main)(["dummy3"]);
+        const templates = [];
+        const recursiveInput = (recurse) => {
+            const input = vscode.window.createInputBox();
+            input.show();
+            console.log("user input => ", input.value);
+        };
+        recursiveInput(true);
+        // main(["dummy3"])
     });
     context.subscriptions.push(disposable);
+    let createTemplate = vscode.commands.registerCommand('no-boilerplate.createTemplate', async () => {
+        vscode.window.showInformationMessage('Create Template');
+        // const input = vscode.window.createInputBox()
+        // input.title = "Enter a name for your template"
+        // input.show()
+        const input = await vscode.window.showInputBox({
+            title: "Enter a name for your template",
+            prompt: "Enter a name for your template",
+            placeHolder: "title",
+        });
+        console.log("user input => ", input);
+        const newFile = await vscode.workspace.openTextDocument({ content: "// template file" });
+        const file = await vscode.window.showTextDocument(newFile);
+        vscode.workspace.onDidOpenTextDocument((e) => { e; });
+        console.log("file content", file);
+    });
+    context.subscriptions.push(createTemplate);
 }
 exports.activate = activate;
 // This method is called when your extension is deactivated
 function deactivate() { }
 exports.deactivate = deactivate;
+/**
+ * INIT FLOW >
+ *
+ * load all the folders from .no-boilerplate
+ * all the folders will be the templates
+ * folder names would be titles for quickPane/quickSelect [ backend-node-ts ]
+ * folder will contain the different templates to be generated on that single call
+ * each file name would be the template title
+ * each file would contain the template details
+ *  folderPath
+ *  filePath
+ *  content
+ *  variable list used
+ *  once a template is selected, all the files and the data will be loaded
+ *  all the vars will be pushed in a single array as key value pairs ( keys being the var names )
+ *  duplicates will be removed from the var array
+ *  the user will be prompted to enter values for the variables, one by one, or all together in a list
+ *  files will generated
+ *
+ */ 
 
 })();
 
--- src/extension.ts
+++ src/extension.ts
@@ -1,9 +1,13 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-import { main } from './main';
+
+import { mkdirSync } from "node:fs";
+import path = require('node:path');
 
 __dirname = __dirname.replace("dist", '')
+
+mkdirSync(path.join(__dirname, ".no-boilerplate"), { recursive: true });
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -20,12 +24,66 @@ export function activate(context: vscode.ExtensionContext) {
     // The code you place here will be executed every time your command is executed
     // Display a message box to the user
     vscode.window.showInformationMessage('Hello World from no-boilerplate!');
-    vscode.workspace.fs
-    main(["dummy3"])
+    const templates = []
+
+    const recursiveInput = (recurse: boolean) => {
+      const input = vscode.window.createInputBox()
+      input.show()
+      console.log("user input => ", input.value)
+    }
+
+    recursiveInput(true)
+
+    // main(["dummy3"])
   });
 
   context.subscriptions.push(disposable);
+
+  let createTemplate = vscode.commands.registerCommand('no-boilerplate.createTemplate', async () => {
+    vscode.window.showInformationMessage('Create Template');
+    // const input = vscode.window.createInputBox()
+    // input.title = "Enter a name for your template"
+    // input.show()
+    const input = await vscode.window.showInputBox({
+      title: "Enter a name for your template",
+      prompt: "Enter a name for your template",
+      placeHolder: "title",
+    });
+    console.log("user input => ", input)
+
+    const newFile = await vscode.workspace.openTextDocument({ content: "// template file" })
+    const file = await vscode.window.showTextDocument(newFile)
+    vscode.workspace.onDidOpenTextDocument((e) => { e })
+    console.log("file content", file)
+
+
+
+  });
+
+  context.subscriptions.push(createTemplate);
 }
 
 // This method is called when your extension is deactivated
 export function deactivate() { }
+
+
+/**
+ * INIT FLOW >
+ * 
+ * load all the folders from .no-boilerplate
+ * all the folders will be the templates
+ * folder names would be titles for quickPane/quickSelect [ backend-node-ts ]
+ * folder will contain the different templates to be generated on that single call
+ * each file name would be the template title
+ * each file would contain the template details
+ *  folderPath
+ *  filePath
+ *  content
+ *  variable list used 
+ *  once a template is selected, all the files and the data will be loaded
+ *  all the vars will be pushed in a single array as key value pairs ( keys being the var names )
+ *  duplicates will be removed from the var array
+ *  the user will be prompted to enter values for the variables, one by one, or all together in a list
+ *  files will generated
+ * 
+ */
